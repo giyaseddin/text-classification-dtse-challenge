e4fd7949ca45b530b45356cbadec6b397ff11ed9
giyaseddin <giyaseddin@innoem.net>
2024-04-16 10:24:43 +0300

    Init data science project - cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experiments and serving for Multi-label Text Classification Model',
+    author='Giyaseddin Bayrak',
+    license='',
+)
